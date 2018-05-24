fe8c9d41e30ce14a20d7847b1523cbf77c5abe26
Julia Westermark <juliawestermark@Julias-MacBook-Pro.local>
2018-05-24 14:46:00 +0200

    pseudo code

--- fast.c
+++ fast.c
@@ -1,17 +1,64 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-struct rational {
+static list_t *freeList;
+
+/** typedef struct rational rat; */
+
+typedef struct rational
+{
 	int p;
 	int q;
-};
+} rat;
 
-typedef struct rational rat;
+/** typedef struct list_t list_t; */
+
+typedef struct list_t
+{
+	list_t *next;
+	list_t *prev;
+	void *data;
+} list_t;
+
+list_t *newList(void *data)
+{
+	list_t *list;
+
+	if (freeList != NULL)
+	{
+		list = freeList;
+		freeList = freeList->next;
+	}
+	else
+	{
+		list = malloc(sizeof(list_t));
+	}
+	assert(list != NULL);
+
+	list->next = list->prev = list;
+	list->data = data;
+
+	return list;
+}
+
+void add(list_t *list, void *data)
+{
+	list_t *link;
+	list_t *temp;
+
+	link = new_list(data);
+
+	list->prev->next = link;
+	link->next = list;
+	temp = list->prev;
+	list->prev = link;
+	link->prev = temp;
+}
 
 rat reduce(rat x)
 {
-	int a=x.p;
-	int b=x.q;
+	int a = x.p;
+	int b = x.q;
 
 	if (a == 0 || b == 0)
 	{
@@ -45,7 +92,7 @@ rat addq(rat x, rat y)
 {
 	rat z;
 
-	z.p = x.p*y.q + y.p*x.q;
+	z.p = x.p * y.q + y.p * x.q;
 	z.q = x.q * y.q;
 
 	z = reduce(z);
@@ -57,7 +104,7 @@ rat subq(rat x, rat y)
 {
 	rat z;
 
-	z.p = x.p*y.q - y.p*x.q;
+	z.p = x.p * y.q - y.p * x.q;
 	z.q = x.q * y.q;
 
 	z = reduce(z);
@@ -98,5 +145,124 @@ rat divq(rat x, rat y)
 bool fm(size_t rows, size_t cols, signed char a[rows][cols], signed char c[rows])
 {
 
-	return 0;
+	int r;
+	int s;
+
+	int n1;
+	int n2;
+
+	list_t pos;
+	list_t neg;
+	list_t zero;
+
+	rat t[rows][cols];
+	rat q[cols];
+
+	r = rows;
+	s = cols;
+
+	/** STEG 1 */
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			t[i][j] = a[i][j];
+			q[j] = c[j];
+		}
+	}
+
+	n1 = 0;
+	n2 = 0;
+
+	for (int j = 0; j < s; j++)
+	{
+		
+		/** STEG 2 */
+		if (t[r][j] > 0)
+		{
+			/** Pusha till början av lista */
+			pos = add(pos,t[][j])
+			n1++;
+			n2++;
+		}
+		else if (t[r][j] < 0)
+		{
+			/** Pusha till slutet av lista */
+			n2++;
+		}
+		else
+		{
+			/** Lägg till sistsist */
+		}
+	}
+	/** STEG 3 */
+	for (int i = 1; i < r - 1)
+	{
+		for (int j = 1; j < n2)
+		{
+			t[i][j] = t[i][j] / t[r][j];
+			q[j] = q[j] / t[r][j];
+			/** Hur gör vi med negativa tal? */
+		}
+	}
+
+	/** STEG 4 */
+	if (n2 > n1)
+	{
+		b[r] = max;
+	}
+	else
+	{
+		b[r] = -inf
+	}
+
+	if (n1 > 0)
+	{
+		B[r] = min;
+	}
+	else
+	{
+		B[r] = inf
+	}
+
+	/** STEG 5 */
+
+	if (r > 1)
+	{
+		/** Gå till steg 6 */
+	}
+	else
+	{ /** r=1 */
+		for (int j = n2 + 1; j <= s; j++)
+		{
+			if (b[1] > B[1] || q[j] < 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return 1;
+			}
+		}
+	}
+
+	/** STEG 6 */
+
+	b[r] <= B[r] Add eq from step 2 sprim = s - n2 + n1 * (n2 - n1);
+	if (sprim > 0)
+	{
+		/** Gå till steg 7 */
+	}
+	else
+	{
+		return 1;
+	}
+
+	/** STEG 7 */
+	r = r - 1;
+	s = sprim;
+	Define new rxs matrix and new s vector
+		/** Gå till steg 2 */
+
+		return 0;
 }
