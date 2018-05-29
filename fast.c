9f3717b038d6b7b5118be3ef85b01658c5690ecd
Julia Westermark <juliawestermark@Julias-MacBook-Pro.local>
2018-05-29 14:35:14 +0200

    .

--- fast.c
+++ fast.c
@@ -212,12 +212,12 @@ bool eliminate(size_t ineq, size_t var, rat t[ineq][var], rat q[ineq])
 
 		if (n1 == n2) {
 			b.q=1;
-			b.p = -2147483648/2;
+			b.p = LONG_MIN/2;
 		}
 
 		if (n1 == 0) {
 			B.q=1;
-			B.p = 2147483647/2;
+			B.p = LONG_MAX/2;
 		}
 
 		pr("var, n1, n2, ineq = %zu, %zu, %zu, %zu", var, n1, n2, ineq);
